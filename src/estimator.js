ca2456a7d7932e05d01109cfedd5ed9a2e59829a
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-04-08 18:41:45 +0300

    modularizing of code

--- src/estimator.js
+++ src/estimator.js
@@ -1,51 +1,13 @@
-import calculator from './mixins/calculator';
+import output from './mixins/output';
 // A month has 30 days
 const covid19ImpactEstimator = (data) => {
-  const impact = {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0
+  const impact = output.getOutput(data , 10);
+  const severeImpact = output.getOutput(data, 50);
+  return {
+    data,
+    impact,
+    severeImpact
   };
-  const severeImpact = {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0
-  };
-
-  impact.currentlyInfected = data.reportedCases * 10;
-
-  impact.infectionsByRequestedTime = calculator.getInfectionsByRequestedTime(
-    impact.currentlyInfected, 28
-  );
-  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime / 0.15);
-
-  impact.hospitalBedsByRequestedTime = calculator.getAvailableHospitalBeds(
-    data.totalHospitalBeds, impact.severeCasesByRequestedTime
-  );
-  impact.casesForICUByRequestedTime = Math.floor(impact.infectionsByRequestedTime / 0.05);
-  impact.casesForVentilatorsByRequestedTime = Math.floor(impact.infectionsByRequestedTime / 0.02);
-  impact.dollarsInFlight = impact.infectionsByRequestedTime
-      * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation * data.timeToElapse;
-
-  severeImpact.currentlyInfected = data.reportedCases * 50;
-
-  severeImpact.infectionsByRequestedTime = calculator.getInfectionsByRequestedTime(
-    severeImpact.currentlyInfected, 28
-  );
-  severeImpact.severeCasesByRequestedTime = Math.floor(
-    severeImpact.infectionsByRequestedTime / 0.15
-  );
-
-  severeImpact.hospitalBedsByRequestedTime = calculator.getAvailableHospitalBeds(
-    severeImpact.severeCasesByRequestedTime, impact.severeCasesByRequestedTime
-  );
-  severeImpact.casesForICUByRequestedTime = Math.floor(
-    severeImpact.infectionsByRequestedTime / 0.05
-  );
-  severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
-    impact.infectionsByRequestedTime / 0.02
-  );
-  severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime
-      * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation * data.timeToElapse;
-  return data;
 };
 
 export default covid19ImpactEstimator;
---
+++ src/mixins/output.js
@@ -0,0 +1,33 @@
+import calculator from './calculator';
+
+const output = {
+
+  getOutput(data, percentage) {
+    const currentlyInfected = data.reportedCases * percentage;
+
+    const infectionsByRequestedTime = calculator.getInfectionsByRequestedTime(
+      currentlyInfected, 28
+    );
+    const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime / 0.15);
+
+    const hospitalBedsByRequestedTime = calculator.getAvailableHospitalBeds(
+      data.totalHospitalBeds, severeCasesByRequestedTime
+    );
+    const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime / 0.05);
+    const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime / 0.02);
+    const dollarsInFlight = infectionsByRequestedTime
+          * data.region.avgDailyIncomeInUSD * data.region.avgDailyIncomePopulation
+        * data.timeToElapse;
+    return {
+      currentlyInfected,
+      infectionsByRequestedTime,
+      severeCasesByRequestedTime,
+      hospitalBedsByRequestedTime,
+      casesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime,
+      dollarsInFlight
+    };
+  }
+
+};
+export default output;
