4926e2ecba24ab3932291108d05217e5c55b74c4
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-04-08 15:28:55 +0300

    setting up challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impact = {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  };
+  const severeImpact = {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  };
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  impact.infectionsByRequestedTime = data.reportedCases * 50;
+
+  severeImpact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.infectionsByRequestedTime = data.reportedCases * 50;
+
+  return data;
+};
 
 export default covid19ImpactEstimator;
